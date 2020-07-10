f1edd41005ca726bd24bad5ad3d3840e634c0b12
Marco <maalanis@miners.utep.edu>
2020-07-10 00:46:49 -0600

    Merging blink and toggle buzzer to p1 and p2

---
+++ demos/button_demo/stateMachines.c
@@ -0,0 +1,77 @@
+#include <msp430.h>
+#include "stateMachines.h"
+#include "buzzer.h"
+
+char toggle_red()		/* always toggle! */
+{
+  static char sound = 0;
+
+  switch (sound) {
+
+  case 0:
+
+    buzzer_set_period(1000);
+    sound = 1;
+    break;
+  case 1:
+    buzzer_set_period(2000);
+    sound = 2;
+    break;
+  case 2:
+    buzzer_set_period(2500);
+    sound = 3;
+    break;
+  case 3:
+    buzzer_set_period(3000);
+    sound = 0;
+    break;
+    
+    
+  }
+
+  return 1;
+
+  /*
+  static char state = 0;
+
+  switch (state) {
+  case 0:
+    red_on = 1;
+    state = 1;
+    break;
+  case 1:
+    red_on = 0;
+    state = 0;
+    break;
+  }
+
+  return 1;			/* always changes an led */
+ 
+}
+
+
+char toggle_green()	/* only toggle green if red is on!  */
+{
+  
+  /*
+  char changed = 0;
+  if (red_on) {
+    green_on ^= 1;
+    changed = 1;
+  }
+  return changed;
+ */
+  return 1;
+}
+
+
+void state_advance()		/* alternate between toggling red & green */
+{
+  
+  toggle_red();
+  
+  
+}
+
+
+
---
+++ demos/button_demo/wdInterruptHandler.c
@@ -0,0 +1,13 @@
+#include <msp430.h>
+#include "stateMachines.h"
+#include "buzzer.h"
+
+void
+__interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
+  static char blink_count = 0;
+  if (++blink_count == 125) {
+    buzzer_set_period(1000);
+    state_advance();
+    blink_count = 0;
+  }
+}
